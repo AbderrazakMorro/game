e727464371956d360fcaa6f92d05a817d3342fd1
AbderrazakMorro <Abderrazakmorro1@gmail.com>
2026-02-27 22:50:36 +0000

    fix: remove unsupported eslint config from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    // Skip ESLint during `next build` so Docker doesn't fail on lint warnings
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-
     // Skip TypeScript type errors during build (we have no tsconfig but just in case)
     typescript: {
         ignoreBuildErrors: true,
